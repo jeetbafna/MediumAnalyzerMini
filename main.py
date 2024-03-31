46b75aed859669632efc799f97e7bf3fe70de0b8
jeetbafna <25198983+jeetbafna@users.noreply.github.com>
2024-03-31 17:26:31 -0500

    Created a Retrieval Vector Class

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import  OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone as pine
 from dotenv import load_dotenv
-import pinecone
+from openai import OpenAI
+from pinecone import Pinecone
+from langchain.chains import RetrievalQA
+
 import os
 
 load_dotenv()
-pinecone.init(api_key = os.environ.get("PINECONE_API_KEY"))
+pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
 if __name__ == "__main__":
     print("Hello everyone")
 
@@ -19,5 +22,10 @@ if __name__ == "__main__":
     print(len(texts))
 
     embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
-    docsearch = Pinecone.from_documents(texts,embeddings, index="medium-blog-embeddings-index")
+    docsearch = pine.from_documents(
+        texts, embeddings, index_name="medium-blog-embeddings-index"
+    )
 
+    qa = RetrievalQA.from_chain_type(
+        llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever()
+    )
