19d64304d57c270d443cc56b6c45b4560edb0c6f
jeetbafna <25198983+jeetbafna@users.noreply.github.com>
2024-03-31 16:46:11 -0500

    Create a text splitter

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from langchain.document_loaders import TextLoader
-
+from langchain.text_splitter import CharacterTextSplitter
 
 if __name__ == "__main__":
     print("Hello everyone")
     loader = TextLoader("./mediumAnalyzer/mediumBlog.txt")
+    document = loader.load()
 
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
