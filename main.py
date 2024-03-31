7d211f33a19064039c82ae2f726605f32eec366e
jeetbafna <25198983+jeetbafna@users.noreply.github.com>
2024-03-31 16:42:45 -0500

    Create text Loader

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from langchain.document_loaders import TextLoader
 
 if __name__ == "__main__":
     print("Hello everyone")
-    loader = TextLoader("./mediumAnalyzer/mediumBlog1.txt")
+    loader = TextLoader("./mediumAnalyzer/mediumBlog.txt")
 
